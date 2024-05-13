e5712e42588daf641c3321b08e23add3bbde5a1c
juri931 <impa.mat@hotmail.it>
2024-05-13 17:11:35 +0200

    [add] table's columns in up()

--- database/migrations/2024_05_13_145939_create_trains_table.php
+++ database/migrations/2024_05_13_145939_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('carriages_number', 3)->unsigned();
+            $table->boolean('is_in_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
             $table->timestamps();
         });
     }
