cb3f2fd6f226e92fc1d418fae976e68726f47575
juri931 <impa.mat@hotmail.it>
2024-05-13 17:27:25 +0200

    [add] slug column after train_code

--- database/migrations/2024_05_13_152241_update_trains_table.php
+++ database/migrations/2024_05_13_152241_update_trains_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->string('slug', 100)->unique()->after('train_code');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->dropColumn('slug');
         });
     }
 };
